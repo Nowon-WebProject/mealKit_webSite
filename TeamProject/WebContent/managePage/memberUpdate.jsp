6c033014594308bc38055370549b3205a500ad37
sunwoooooo <sunwookim95@gmail.com>
2022-05-21 21:07:10 +0900

    관리자페이지

--- TeamProject/WebContent/managePage/memberUpdate.jsp
+++ TeamProject/WebContent/managePage/memberUpdate.jsp
@@ -1,13 +1,14 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+<%@ page import= "kr.co.EZHOME.domain.User" %>
 <!DOCTYPE html>
 <!-- 회원가입 페이지 -->
 <html>
 <head>
 <meta charset="UTF-8">
-<title>이젠, 집에서 | 회원가입</title>
+<title>이젠, 집에서 | 회원수정</title>
 <link href="/TeamProject/css/styles.css?test1=5" rel="stylesheet" />
-    <title>회원가입 화면</title>
+    <title>회원수정 화면</title>
     
     <style>
         #wrap{
@@ -39,40 +40,32 @@
     <script src="http://dmaps.daum.net/map_js_init/postcode.v2.js"></script>
 </head>
 <body>
+<%
+	User bean=(User)request.getAttribute("bean");
+	String[] arr=(String[])request.getAttribute("arr");
+%>
 <jsp:include page="/ui/nav.jsp"></jsp:include>
 <!-- 왼쪽, 오른쪽 바깥여백을 auto로 주면 중앙정렬된다.  -->
     <div align="center">
         <br><br>
-        <b><font size="6" color="gray">회원가입</font></b>
+        <b><font size="6" color="gray">회원수정</font></b>
         <br><br><br>
     </div>
 <div id="wrap">
-	<form action="/TeamProject/join.do" method="post" name="frm">
+	<form action="/TeamProject/memberUpdate.do" method="post" name="frm">
 		<table>
 			<tr>
 			<td colspan="2"><hr> </td>
 			</tr>
 			<tr>
 				<td>이름</td>
-				<td><input type="text" name="name" size="20" maxlength="16">*</td>
+				<td><input type="text" name="name" size="20" maxlength="16" value=<%=bean.getName() %>>*</td>
 			</tr>
 			<tr>
 				<td>아이디</td>
 				<td>
-					<input type="text" id="userid" name="userid" size="20" maxlength="16">*
-					<input type="hidden" name="reid" size="20">
-					<input type="button" value="중복체크" onclick="idCheck()">
-				</td>
-			</tr>
-			<tr>
-				<td/>
-				<td>
-					<p class="txt_guideId" >
-						<span class="badId">4자 이상의 영문 혹은 영문과 숫자를 조합</span>
-						<input type="hidden" name="idValid" size="20">
-						<br>
-						<span class="badCheckedId">아이디 중복확인</span>
-					</p>
+					<%=bean.getUserid() %>
+					<input type="hidden" name="userid" size="20" value=<%=bean.getUserid() %>>
 				</td>
 			</tr>
 			<tr>
@@ -106,7 +99,7 @@
 			<tr>
 				<td>생년월일</td>
 				<td>
-					<input type="text" id="birth" name="birth" placeholder="8자리입력(예.19951129)" maxlength="8">
+					<input type="text" id="birth" name="birth" value=<%=bean.getBirth() %> maxlength="8">
 				</td>
 			</tr>
 			<tr>
@@ -120,23 +113,23 @@
 			</tr>
 			<tr>
 				<td>이메일</td>
-				<td><input type="text" name="email" size="20"></td>
+				<td><input type="text" name="email" size="20" value=<%=bean.getEmail() %>></td>
 			</tr>
 			<tr>
 				<td>전화번호</td>
 				<td>
-					<input type='tel' class="phone" name='phone' maxlength="13"/>
+					<input type='tel' class="phone" name='phone' value=<%=bean.getPhone() %> maxlength="13"/>
 				</td>
 			</tr>
 			<tr>
 				<td>주소</td>
 				<td>
-					<input type="text" id="sample4_postcode" name="addr1" placeholder="우편번호" readonly>
+					<input type="text" id="sample4_postcode" name="addr1" value="<%=arr[0] %>" readonly>
 					<input type="button" onclick="sample4_execDaumPostcode()" value="우편번호 찾기"><br>
-					<input type="text" id="sample4_roadAddress" name="roadAddr" placeholder="도로명주소" size="60" readonly><br>
+					<input type="text" id="sample4_roadAddress" name="roadAddr" value="<%=arr[1] %>" size="60" readonly><br>
 					<input type="hidden" id="sample4_jibunAddress" name="jibunAddr" placeholder="지번주소"  size="60">
 					<span id="guide" style="color:#999;display:none"></span> 
-					<input type="text" id="sample4_detailAddress" name="addr3" placeholder="상세주소"  size="60"><br>
+					<input type="text" id="sample4_detailAddress" name="addr3" value="<%=arr[2] %>"  size="60"><br>
 					<input type="hidden" id="sample4_extraAddress" name="cham" placeholder="참고항목"  size="60">
 					<input type="hidden" id="sample4_engAddress" name="engAddr" placeholder="영문주소"  size="60" >
 				</td>
@@ -150,13 +143,11 @@
 			<tr>
 				<td colspan="2" align="center">
 					<input type="hidden" name="admin" value="0">
-					<input type="submit" value="확인" onclick="return joinCheck()">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-					<input type="reset" value="취소">
+					<input type="submit" value="수정">
+					<input type="reset" value="취소" onclick="history.back()">
 				</td>
 			</tr>
-			<tr>
-				<td colspan="2">${message}</td>
-			</tr>
+		
 		</table>
 	</form>
 </div>
--- TeamProject/src/kr/co/EZHOME/dao/UserDAO.java
+++ TeamProject/src/kr/co/EZHOME/dao/UserDAO.java
@@ -5,6 +5,7 @@ import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Vector;
 
 import javax.naming.Context;
 import javax.naming.InitialContext;
@@ -30,10 +31,10 @@ public class UserDAO {
 	public Connection getConnection() throws Exception {
 		Connection conn = null;
 		Context initContext = new InitialContext();
-		Context envContext=(Context)initContext.lookup("java:/comp/env");
-		DataSource ds =(DataSource)envContext.lookup("jdbc/Oracle11g");
-		conn=ds.getConnection();
-		
+		Context envContext = (Context) initContext.lookup("java:/comp/env");
+		DataSource ds = (DataSource) envContext.lookup("jdbc/Oracle11g");
+		conn = ds.getConnection();
+
 		return conn;
 	}
 	
@@ -216,7 +217,7 @@ public class UserDAO {
 	
 	public int updateMember(UserDTO udto) {
 		int result=-1;
-		String sql="update usertbl set pwd=?,email=?,phone=?,admin=? where userid=?";
+		String sql="update usertbl set name = ?, pwd = ?, email = ?, phone = ?, birth = ?, addr = ? where userid = ?";
 		
 		Connection conn=null;
 		PreparedStatement pstmt=null;
@@ -224,11 +225,13 @@ public class UserDAO {
 		try {
 			conn=getConnection();
 			pstmt=conn.prepareStatement(sql);
-			pstmt.setString(1,udto.getPwd());
-			pstmt.setString(2,udto.getEmail());
-			pstmt.setString(3,udto.getPhone());
-			pstmt.setInt(4,udto.getAdmin());
-			pstmt.setString(5,udto.getUserid());
+			pstmt.setString(1,udto.getName());
+			pstmt.setString(2,udto.getPwd());
+			pstmt.setString(3,udto.getEmail());
+			pstmt.setString(4,udto.getPhone());
+			pstmt.setString(5,udto.getBirth());
+			pstmt.setString(6,udto.getAddr());
+			pstmt.setString(7,udto.getUserid());
 			
 			result=pstmt.executeUpdate();//
 			System.out.println("result="+result);
@@ -248,4 +251,142 @@ public class UserDAO {
 		return result;
 	}
 
+	public Vector<UserDTO> allselect(){ 
+		
+		Vector<UserDTO> vec=new Vector<UserDTO>();
+		String sql = "select * from usertbl";
+		
+		Connection conn=null;
+		PreparedStatement ptmt=null;
+		ResultSet rs=null;
+		
+		try {
+			conn=getConnection();
+			ptmt=conn.prepareStatement(sql);
+			rs=ptmt.executeQuery();
+			
+			while(rs.next()) {
+				UserDTO udto=new UserDTO();
+				udto.setName(rs.getString("name"));
+				udto.setUserid(rs.getString("userid"));
+				udto.setPwd(rs.getString("pwd"));
+				udto.setEmail(rs.getString("email"));
+				udto.setPhone(rs.getString("phone"));
+				udto.setAddr(rs.getString("addr"));
+				udto.setDeliAddr(rs.getString("deli"));
+				udto.setPoint(rs.getInt("point"));
+				udto.setAdmin(rs.getInt("admin"));
+				
+				if(rs.getDate("birth") == null) {
+					udto.setBirth(null);
+				}else {udto.setBirth(rs.getDate("birth").toString());}
+				
+				if(rs.getDate("registdate") == null) {
+					udto.setRegistDate(null);
+				}else {udto.setRegistDate(rs.getDate("registdate").toString());}
+				
+				vec.add(udto);
+			}
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+		}finally {
+			try {
+			if( rs != null ) 
+				rs.close();
+			if( ptmt != null)
+				ptmt.close();
+			if( conn != null)
+				conn.close();
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		}
+	
+		return vec;
+	}
+	
+	public int deleteMember(String userid) {
+		int answer=-1;
+		Connection conn = null;
+		PreparedStatement ptmt = null;
+		
+		String sql="delete from usertbl where userid = ?";
+		
+		try {
+			conn=getConnection();
+			ptmt=conn.prepareStatement(sql);
+			ptmt.setString(1, userid);
+			answer=ptmt.executeUpdate();
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+		}finally{
+			try {
+				if( conn != null) 
+					conn.close();
+				if( ptmt != null)
+					ptmt.close();
+			}catch(Exception e) {
+				e.printStackTrace();
+			}
+		}
+		return answer;
+	}
+	
+	public Vector<UserDTO> likeFind(String type,String key){
+		Vector<UserDTO> vec=new Vector<UserDTO>();
+		
+		Connection conn=null;
+		PreparedStatement ptmt=null;
+		ResultSet rs=null;
+		
+		String sql="select * from usertbl where "+type+" like "+"'%"+key+"%'";
+		
+		try {
+			conn=getConnection();
+			ptmt=conn.prepareStatement(sql);
+			rs=ptmt.executeQuery();
+			
+			while(rs.next()) {
+				UserDTO udto=new UserDTO();
+				udto.setName(rs.getString("name"));
+				udto.setUserid(rs.getString("userid"));
+				udto.setPwd(rs.getString("pwd"));
+				udto.setEmail(rs.getString("email"));
+				udto.setPhone(rs.getString("phone"));
+				udto.setAddr(rs.getString("addr"));
+				udto.setDeliAddr(rs.getString("deli"));
+				udto.setPoint(rs.getInt("point"));
+				udto.setAdmin(rs.getInt("admin"));
+				
+				if(rs.getDate("birth") == null) {
+					udto.setBirth(null);
+				}else {udto.setBirth(rs.getDate("birth").toString());}
+				
+				if(rs.getDate("registdate") == null) {
+					udto.setRegistDate(null);
+				}else {udto.setRegistDate(rs.getDate("registdate").toString());}
+				
+				vec.add(udto);
+			}
+			
+				
+		}catch(Exception e) {
+			e.printStackTrace();
+		}finally {
+			try {
+				if( rs != null)
+					rs.close();
+				if( ptmt != null)
+					ptmt.close();
+				if( conn != null)
+					conn.close();
+			}catch(Exception e) {
+				e.printStackTrace();
+			}
+		}
+		return vec;
+	}
 }
+
